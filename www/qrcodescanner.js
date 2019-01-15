7afa5b72ce0907566bdf47f6e6566da6f74d7c9b
Jan Scheurenbrand <mail@janscheurenbrand.de>
2019-01-15 15:55:12 +0000

    add getImage to qrcodescanner js stub

--- www/qrcodescanner.js
+++ www/qrcodescanner.js
@@ -4,6 +4,10 @@ exports.startScan = function (x, y, width, height, success, error) {
     exec(success, error, "QRCodeScanner", "startScan", [x, y, width, height]);
 };
 
+exports.getImage = function (x, y, width, height, success, error) {
+  exec(success, error, "QRCodeScanner", "getImage", [x, y, width, height]);
+};
+
 exports.stopScan = function (success, error) {
     exec(success, error, "QRCodeScanner", "stopScan", []);
 };
